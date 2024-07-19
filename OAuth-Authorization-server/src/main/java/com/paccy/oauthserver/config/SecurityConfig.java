b53264bf10491e840112ae151e0b7c22b43ddbdf
PaccyC <abayisengaaimepacifique@gmail.com>
2024-07-19 10:35:31 +0200

    configured authServerSecurityFilterChain

---
+++ OAuth-Authorization-server/src/main/java/com/paccy/oauthserver/config/SecurityConfig.java
@@ -0,0 +1,33 @@
+package com.paccy.oauthserver.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.server.authorization.config.annotation.web.configuration.OAuth2AuthorizationServerConfiguration;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration(
+        proxyBeanMethods = false
+)
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+
+
+    private final PasswordEncoder passwordEncoder;
+
+    @Bean
+    @Order(Ordered.HIGHEST_PRECEDENCE)
+    public SecurityFilterChain authServerSecurityFilterChain(HttpSecurity http) throws Exception{
+        OAuth2AuthorizationServerConfiguration.applyDefaultSecurity(http);
+
+        return  http.formLogin(Customizer.withDefaults()).build();
+    }
+
+}
--- OAuth-Authorization-server/src/main/java/com/paccy/oauthserver/service/CustomUserDetailsService.java
+++ OAuth-Authorization-server/src/main/java/com/paccy/oauthserver/service/CustomUserDetailsService.java
@@ -1,14 +1,30 @@
 package com.paccy.oauthserver.service;
 
+import com.paccy.oauthserver.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class CustomUserDetailsService implements UserDetailsService {
+
+
+    private final UserRepository userRepository;
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return null;
+        return this.userRepository.findByUsername(username)
+            .orElseThrow(()-> new UsernameNotFoundException("User Not Found!"));
+    }
+
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return  new BCryptPasswordEncoder(11);
     }
 }
